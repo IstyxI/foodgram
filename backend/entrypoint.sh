435e1cc5f421e5f708369f8d673947d697d39a5f
xDELETEDx <andrewow2030@gmail.com>
2024-12-18 20:10:40 +0300

    4

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
+set -e
+cd foodgram
 while ! nc -z db 5432;
     do sleep .5;
     echo "wait database";
 done;
     echo "connected to the database";
 
-python manage.py migrate;
-python manage.py collectstatic --noinput;
-gunicorn -w 2 -b 0:8000 foodgram.wsgi;
+python manage.py collectstatic --no-input
+python manage.py migrate
+exec "$@"
