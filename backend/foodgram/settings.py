617003fdc8eb99fa6a7623a3e51d6c10a2a8ac86
xDELETEDx <andrewow2030@gmail.com>
2024-12-18 19:37:22 +0300

    1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -57,7 +57,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('POSTGRES_DB', 'postgres'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
