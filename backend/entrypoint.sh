2fe79f4188f6043181d4beea5800d1d64ed1bd77
xDELETEDx <andrewow2030@gmail.com>
2024-12-30 23:50:45 +0300

    ыыыыыы блекформатер делает """""""""""))))

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,8 +2,7 @@
 set -e
 export PYTHONPATH=/app:$PYTHONPATH
 cd /app/backend
-python manage.py makemigrations users
-python manage.py makemigrations api
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py load_ingredients
