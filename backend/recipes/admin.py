499144c49b3695d004e25774a5bc737e593fee35
xDELETEDx <andrewow2030@gmail.com>
2025-01-15 01:26:09 +0300

    admin page optimization

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -64,7 +64,7 @@ class FollowAdmin(ModelAdmin):
 @register(Favorite)
 class FavoriteAdmin(ModelAdmin):
     list_display = ("pk", "user", "recipe")
-    list_display_links = ("user", "recipe")
+    list_display_links = ("user", )
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
